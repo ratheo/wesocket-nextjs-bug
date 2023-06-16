f1dc541ff437faf214cd83c69984015abfc4a1b5
ratheo <rath@tv2.dk>
2023-06-16 16:22:27 +0200

    minimal repro example

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  rewrites: async () => [
+    {
+      source: "/websocket",
+      destination: "/Rewrite",
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
